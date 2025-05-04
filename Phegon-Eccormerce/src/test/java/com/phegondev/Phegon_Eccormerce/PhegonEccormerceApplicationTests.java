3f2f7a0f9e6c08f8b7e7db755a1cf9b8af5773fe
Md Neyaz Ahmad <27228619+neyaz1@users.noreply.github.com>
2025-05-04 21:39:44 +0530

    Add files via upload
    first commited

---
+++ Phegon-Eccormerce/src/test/java/com/phegondev/Phegon_Eccormerce/PhegonEccormerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phegondev.Phegon_Eccormerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhegonEccormerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
