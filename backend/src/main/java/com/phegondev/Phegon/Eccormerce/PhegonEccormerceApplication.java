bf60d71fbe8a693f05a4a87f681f6e0d2fde9288
neyaz1 <neyins@gmail.com>
2025-05-05 23:51:09 +0530

    chage entry and pom file

--- backend/src/main/java/com/phegondev/Phegon/Eccormerce/PhegonEccormerceApplication.java
+++ backend/src/main/java/com/phegondev/Phegon/Eccormerce/PhegonEccormerceApplication.java
@@ -2,9 +2,16 @@ package com.phegondev.Phegon.Eccormerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PhegonEccormerceApplication {
+	@GetMapping("/messgae")
+	String getMessage(){
+		return  "Welcome";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhegonEccormerceApplication.class, args);
